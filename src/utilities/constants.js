ec88cffd14f12b8634a975dd69bcbc750017cfa8
Mayank Yadav <mayank4code@gmail.com>
2024-06-26 17:55:59 +0530

    versal deploy

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,6 @@
 // const server_origin = "https://api.udyamuplift.in";
-const server_origin = 'http://localhost:5000';
+// const server_origin = 'http://localhost:5000';
+const server_origin = 'https://udyam-uplift-server.vercel.app';
 
 
 export {server_origin};
