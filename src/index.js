459b3243e2996aab101f48992341d21b6d98ed93
Mayank Yadav <mayank4code@gmail.com>
2024-07-28 08:43:41 +0530

    vercel-insight-removed

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LanguageProvider } from './context/LanguageContext';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 //   <React.StrictMode>
-        <SpeedInsights>
-
-                <LanguageProvider>
-                        <App />
-                </LanguageProvider>
-
-        </SpeedInsights>
-        
+        <LanguageProvider>
+                <App />
+        </LanguageProvider>
 //   {/* </React.StrictMode> */}
 );
 
